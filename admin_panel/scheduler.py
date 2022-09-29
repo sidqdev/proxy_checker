d5c470b243e6cabae0c3db162ee71eba2fe16343
sidq-corp <dimax258223@gmail.com>
2022-09-29 14:31:49 +0300

    proxy try

--- admin_panel/scheduler.py
+++ admin_panel/scheduler.py
@@ -1,7 +1,7 @@
 import os
 import time
 import requests
-import requests.auth
+from requests_digest_proxy import HTTPProxyDigestAuth
 
 from threading import Thread
 
@@ -9,40 +9,6 @@ from apscheduler.schedulers.background import BackgroundScheduler
 from telebot import TeleBot
 
 from .models import Proxy, Settings
-
-
-class HTTPProxyDigestAuth(requests.auth.HTTPDigestAuth):
-    last_nonce = True
-    def handle_407(self, r):
-        """Takes the given response and tries digest-auth, if needed."""
-
-        num_407_calls = r.request.hooks['response'].count(self.handle_407)
-
-        s_auth = r.headers.get('Proxy-authenticate', '')
-
-        if 'digest' in s_auth.lower() and num_407_calls < 2:
-
-            self.chal = requests.auth.parse_dict_header(s_auth.replace('Digest ', ''))
-
-            # Consume content and release the original connection
-            # to allow our new request to reuse the same one.
-            r.content
-            r.raw.release_conn()
-
-            r.request.headers['Authorization'] = self.build_digest_header(r.request.method, r.request.url)
-            r.request.send(anyway=True)
-            _r = r.request.response
-            _r.history.append(r)
-
-            return _r
-
-        return r
-
-    def __call__(self, r):
-        if self.last_nonce:
-            r.headers['Proxy-Authorization'] = self.build_digest_header(r.method, r.url)
-        r.register_hook('response', self.handle_407)
-        return r
 
 
 def send_notification(proxy: Proxy):
