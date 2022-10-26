2935dc5cda9cc435a13fe041cbe81df87ca5bd96
sidq-corp <dimax258223@gmail.com>
2022-10-26 22:25:19 +0300

    try

--- proxy_checker/settings.py
+++ proxy_checker/settings.py
@@ -28,11 +28,13 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 CSRF_TRUSTED_ORIGINS = ['http://*']
+CORS_ALLOWED_ORIGINS = ['*']
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'admin_panel',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -43,10 +45,12 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    # 'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
