4e27c54caa968c5cb911208684d8a59ff9ecebe3
sidq-corp <dimax258223@gmail.com>
2022-10-26 22:43:36 +0300

    fix

--- admin_panel/views.py
+++ admin_panel/views.py
@@ -8,7 +8,6 @@ import json
 
 
 def get_ip_address(request):
-    print(request.headers)
     user_ip_address = request.headers.get('HTTP_X_FORWARDED_FOR')
     if user_ip_address:
         ip = user_ip_address.split(',')[0]
