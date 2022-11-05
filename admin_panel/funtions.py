c6fb86fcd3a5bcef969a45aa528cafb7aed69b79
sidq-corp <dimax258223@gmail.com>
2022-11-05 23:13:29 +0200

    test

--- admin_panel/funtions.py
+++ admin_panel/funtions.py
@@ -86,6 +86,7 @@ def get_last_sms(proxy: Proxy):
     proxies_config = {'proxies': proxies, 'auth': auth}
     ctx = api_user.quick_login(proxy.modem_username, proxy.modem_password, modem_host="192.168.8.1", proxies_config=proxies_config)
     sms_ = sms.get_sms(ctx)
+    print(sms_)
     if not sms_:
         return ''
     
